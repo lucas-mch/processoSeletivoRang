1aa9b0b9244005e0e085cb50c86fdaaf1a70e034
Lucas Machado de Souza <lcsmachaado@gmail.com>
2024-03-29 03:43:53 -0300

    First tests in UnidadeSaudeBean.java

--- src/main/java/dev/lucasmachado/rangprocessoseletivo/bean/UnidadeSaudeBean.java
+++ src/main/java/dev/lucasmachado/rangprocessoseletivo/bean/UnidadeSaudeBean.java
@@ -1,7 +1,9 @@
 package dev.lucasmachado.rangprocessoseletivo.bean;
 
 import dev.lucasmachado.rangprocessoseletivo.model.UnidadeSaude;
+import dev.lucasmachado.rangprocessoseletivo.service.UnidadeSaudeService;
 import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Inject;
 import jakarta.inject.Named;
 
 import java.util.ArrayList;
@@ -11,27 +13,43 @@ import java.util.List;
 @RequestScoped
 public class UnidadeSaudeBean {
 
-    private List<UnidadeSaude> unidades;
+    @Inject
+    private UnidadeSaudeService unidadeSaudeService;
 
-    public List<UnidadeSaude> getUnidades() {
-        if (unidades == null) {
-            unidades = new ArrayList<>();
-            unidades.add(new UnidadeSaude(1L, "1", "Primeiro", 10000, 20000));
-            unidades.add(new UnidadeSaude(2L, "2", "Segundo", 30000, 40000));
-            unidades.add(new UnidadeSaude(3L, "3", "Terceiro", 40000, 50000));
-            unidades.add(new UnidadeSaude(4L, "4", "Quarto", 50000, 60000));
-            unidades.add(new UnidadeSaude(5L, "5", "Quinto", 60000, 70000));
-            unidades.add(new UnidadeSaude(6L, "6", "Sexto", 70000, 80000));
-            unidades.add(new UnidadeSaude(7L, "7", "Sétimo", 80000, 90000));
-            unidades.add(new UnidadeSaude(8L, "8", "Oitavo", 90000, 100000));
-            unidades.add(new UnidadeSaude(9L, "9", "Nono", 100000, 110000));
-            unidades.add(new UnidadeSaude(10L, "10", "Décimo", 110000, 120000));
-        }
-        return unidades;
+    private List<UnidadeSaude> unidades;
+    private UnidadeSaude unidadeSaude = new UnidadeSaude();
+    private UnidadeSaude selectedUnidade;
+    public UnidadeSaude getSelectedUnidade() {
+        return selectedUnidade;
+    }
+    public void setSelectedUnidade(UnidadeSaude selectedUnidade) {
+        this.selectedUnidade = selectedUnidade;
     }
 
     public void setUnidades(List<UnidadeSaude> unidades) {
         this.unidades = unidades;
     }
 
+    public void deleteUnidade() {
+        unidades.remove(selectedUnidade);
+        selectedUnidade = null;
+    }
+
+    public void viewUnidade() {
+    }
+
+    public UnidadeSaude getUnidadeSaude() {
+        return unidadeSaude;
+    }
+
+    public void setUnidadeSaude(UnidadeSaude unidadeSaude) {
+        this.unidadeSaude = unidadeSaude;
+    }
+    public void salvarUnidadeSaude() {
+        unidadeSaudeService.salvar(this.unidadeSaude);
+    }
+
+    public void findAll() {
+        unidadeSaudeService.findAll();
+    }
 }
