0b42e4269694a3283a1be79adda44cc73aacd0a6
Lucas Machado de Souza <lcsmachaado@gmail.com>
2024-04-07 08:45:34 -0300

    Build image

--- src/main/java/dev/lucasmachado/rangprocessoseletivo/validator/UnidadeSaudeFormValidator.java
+++ src/main/java/dev/lucasmachado/rangprocessoseletivo/validator/UnidadeSaudeFormValidator.java
@@ -10,23 +10,23 @@ import jakarta.faces.validator.Validator;
 import jakarta.faces.validator.ValidatorException;
 import jakarta.inject.Inject;
 
-@FacesValidator(value="unidadeSaudeForm")
+@FacesValidator(value = "unidadeSaudeForm")
 public class UnidadeSaudeFormValidator implements Validator {
 
     @Inject
     private NotificationBean notificationBean;
+
     @Override
     public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
         UIInput faixaCepInicio = (UIInput) component.getAttributes().get("faixaCepInicio");
         UIInput faixaCepFinal = (UIInput) component.getAttributes().get("faixaCepFinal");
 
-
         Integer cepInicio = (Integer) faixaCepInicio.getSubmittedValue();
-        Integer cepFinal = (Integer) faixaCepInicio.getSubmittedValue();
+        Integer cepFinal = (Integer) faixaCepFinal.getSubmittedValue();
 
-        if(cepInicio > cepFinal) {
+        if (cepFinal > cepInicio) {
             String error = "O valor final da faixa de cep não pode ser maior que o inicial.";
-            notificationBean.addMessage(FacesMessage.SEVERITY_ERROR,error,error);
+            notificationBean.addMessage(FacesMessage.SEVERITY_ERROR, error, error);
         }
     }
 }
